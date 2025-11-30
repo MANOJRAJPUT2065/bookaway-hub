46fd1fcf43326fba96804bcebbc5797b773b7b20
MANOJ SINGH RAJPUT <131298877+MANOJRAJPUT2065@users.noreply.github.com>
2025-11-30 12:56:51 +0530

    Add bookings routes to the backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
+import bookingsRoutes from "./routes/bookings.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/bookings", bookingsRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend server is running");
